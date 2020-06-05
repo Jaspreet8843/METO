23ae223bba76e4536e01add8a5840132b504b984
Jaspreet8843 <Jaspreet8843@users.noreply.github.com>
2020-06-05 11:14:56 +0530

    navbar animation

--- scrpt.js
+++ scrpt.js
@@ -5,8 +5,25 @@ $(window).resize(function(){
    if(width <1024){
 
    	$('fspace').addClass('w-100');
-   	console.log(width);
    }
    
 })
 .resize();//trigger the resize event on page load.
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myModal").style.top = "0";
+  } else {
+    document.getElementById("myModal").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+setTimeout(fade_out, 3000);
+
+function fade_out() {
+  $('#myModal').modal('hide');
+}
