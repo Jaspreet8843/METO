a8536e184df43f4293c5ac3a360a93edad8f52d0
Jaspreet8843 <Jaspreet8843@users.noreply.github.com>
2020-06-12 14:52:30 +0530

    made header footer common

--- meto_user_app/views.py
+++ meto_user_app/views.py
@@ -3,26 +3,28 @@ from django.http import HttpResponse
 
 # Create your views here.
 def login(request):
-	return render(request,'login.html')
+	return render(request,'customer/login.html')
 
 def signup(request):
 	return HttpResponse("Under construction")
 
 def index(request):
-	return render(request,'index.html')
+	return render(request,'customer/index.html')
 
 def profile(request):
-	return render(request,'profile.html')
+	return render(request,'customer/profile.html')
 
 def edit_profile(request):
-	return render(request,'edit_profile.html')
+	return render(request,'customer/edit_profile.html')
 
 def book(request,service_id):
 	return HttpResponse("Under construction")
 
 def bookings(request):
-	return render(request,'bookings.html')
+	return render(request,'customer/bookings.html')
 
+def book_ticket(request):
+    return render(request, 'customer/book_ticket.html')
 def feedback(request):
 	return HttpResponse("Under construction")
 
