46bbb871f035520ec73d746d6d8b1f8dbda3da46
Jaspreet8843 <Jaspreet8843@users.noreply.github.com>
2020-06-13 13:18:01 +0530

    test

--- meto/settings.py
+++ meto/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 """
 Django settings for meto project.
 
@@ -87,8 +88,8 @@ DATABASES = {
     }
 }
 
-#db_from_env = dj_database_url.config()
-#DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
