fb8d19aca2aacb14fa6d8bd18cbb6ee8f723146b
Manab <46393531+manabsaha@users.noreply.github.com>
2020-06-19 14:07:09 +0530

    assign bookings

--- meto_admin_app/urls.py
+++ meto_admin_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
 	path('',views.admin_index,name="admin_index"),
 	path('admin_login/',views.admin_login,name="admin_login"),
 	path('admin_logout/',views.admin_logout,name="admin_logout"),
+	path('assign_worker/<int:service_id>',views.assign_worker,name="assign_worker"),
+	path('assign_staff/',views.assign_staff,name="assign_staff"),
 ]
--- meto_admin_app/views.py
+++ meto_admin_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import staff,worker,assign,date
 from meto_user_app.models import user, booking, service, recovery, feedback
 from .validator import valid_login
+from datetime import datetime
 
 # Create your views here.
 def admin_index(request):
@@ -46,3 +47,42 @@ def admin_login(request) :
 def admin_logout(request):
 	del request.session['staff_id']
 	return redirect('admin_login')
+
+def assign_worker(request,service_id):
+	if request.method=='POST':
+		worker_id = request.POST.get('worker_id')
+		booking_id = request.POST.get('booking_id')
+		visiting_date = request.POST.get('visiting_date')
+		technician_assigned_date = datetime.now().strftime('%Y-%m-%d')
+		worker_obj = worker.objects.get(worker_id=worker_id)
+		booking_obj = booking.objects.get(booking_id=booking_id)
+		assign_obj = assign(worker_id=worker_obj,booking_id=booking_obj)
+		date_obj = date.objects.filter(booking_id=booking_obj)
+		date_obj.update(visiting_date=visiting_date,technician_assigned_date=technician_assigned_date)
+		return HttpResponse("assigned")
+	service_obj = service.objects.get(service_id=service_id)
+	booking_obj = booking.objects.filter(service_id=service_obj,booking_status="Processing")
+	worker_obj = worker.objects.filter(service_id=service_obj)
+	# return render(request,'management/assign_worker.html',({'bookings':booking_obj,'workers':worker_obj})
+	return HttpResponse("assign worker")
+
+def workers(request):
+	worker_obj = worker.objects.all()
+	# return render(request,'management/workers.html',({'workers':worker_obj}))
+	return HttpResponse("all workers")
+
+def assign_staff(request):
+	if request.method=='POST':
+		booking_id = request.POST.get('booking_id')
+		staff_id = request.POST.get('staff_id')
+		staff_assigned_date = datetime.now().strftime('%Y-%m-%d')
+		booking_obj = booking.objects.get(booking_id=booking_id)
+		staff_obj = booking.objects.get(staff_id=staff_id)
+		assign_obj = assign(booking_id=booking_obj,staff_id=staff_obj)
+		date_obj = date.objects.filter(booking_id=booking_obj)
+		date_obj.update(staff_assigned_date=staff_assigned_date)
+		return HttpResponse("assigned")
+	booking_obj = booking.objects.all()
+	staff_obj = staff.objects.all()
+	# return render(request,'management/assign_staff.html',({'bookings':booking_obj,'staffs':staff_obj}))
+	return HttpResponse("assign staff")
